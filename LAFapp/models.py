b8b26598e4517772d1aa2dc884b3de8ae1dc7d9d
haunguyen1312 <hau@docosan.com>
2025-08-15 23:58:47 +1000

    Create model Items

--- LAFapp/models.py
+++ LAFapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Items(models.Model):
+	ID = models.AutoField(auto_created = True, primary_key=True, verbose_name="ID")
+	status = models.CharField(null=False,blank=True)
+	description = models.TextField(null=True,blank=True)
+	location = models.TextField(null=True,blank=True)
+	email = models.EmailField(null=False,blank=True)
+	date = models.DateField(null=True,blank=True)
+
+	def __str__(self):
+		return f"{self.status}"
