e39848f22b551e0447036cb54a93a62523e196e3
haunguyen1312 <hau@docosan.com>
2025-08-22 20:15:41 +1000

    delete form and run tests

--- LAFapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Items
-
-class ItemsForm(forms.ModelForm):
-    class Meta:
-        model = Items
-        fields = ['status', 'description', 'location', 'email', 'date', 'category', 'key_word']
